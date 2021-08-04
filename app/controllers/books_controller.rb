56586d3b6b3415f52986560e3201078034303dc6
yyuu2479 <yyuu2479@gmail.com>
2021-08-04 13:32:10 +0000

    bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+    def index
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def create
+    end
+    
+    def destroy
+    end
+    
+    def update
+    end
+end
