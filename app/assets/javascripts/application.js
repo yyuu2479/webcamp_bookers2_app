a42257b1d38cce15c75524a6f2683cd0554f0b80
yyuu2479 <yyuu2479@gmail.com>
2021-08-05 06:10:44 +0000

    bookers2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery3
 //= require popper
-//= require bootstrap-sprokets
+//= require bootstrap-sprockets
 // 
 //= require rails-ujs
 //= require activestorage
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
     def index
+        @user = User.all
     end
     
     def show
+        
     end
     
     def edit
