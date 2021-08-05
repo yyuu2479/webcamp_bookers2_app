e3e6800b61d9fc81e10a0b4db3ddf45a0428dbb1
yyuu2479 <yyuu2479@gmail.com>
2021-08-05 16:03:00 +0000

    Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,33 @@
 class BooksController < ApplicationController
     def index
+        @books = Book.all
+        @book = Book.new
     end
-    
+
     def show
     end
-    
+
     def edit
     end
-    
+
     def create
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        @book.save
+        redirect_to books_path
     end
-    
+
     def destroy
     end
-    
+
     def update
     end
+    
+    
+    private
+    
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+    
 end
