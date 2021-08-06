c2e881f43b4a1e9d16b186c2bf21335ca4177636
yyuu2479 <yyuu2479@gmail.com>
2021-08-06 15:29:28 +0000

    Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,16 @@ class UsersController < ApplicationController
     end
     
     def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        redirect_to user_path(user.id)
+    end
+    
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :introduction, :profile_image)
     end
     
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7fc4084b6977ba9e3db1aa4f69b569029de2edb9239b1af2c5eb51f3259960ee45c7822f96229c304e7110f958acaf6367eee4e7d7d15ff5a79063f33b6badfc'
