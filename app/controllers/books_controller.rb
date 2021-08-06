8b500537668d117f51e413a08cf029337add0e86
yyuu2479 <yyuu2479@gmail.com>
2021-08-06 13:01:11 +0000

    Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,40 @@
 class BooksController < ApplicationController
     def index
-        @books = Book.all
         @book = Book.new
+        @books = Book.all
     end
 
     def show
+        @book = Book.new
+        @books = Book.find(params[:id])
     end
 
     def edit
+        @book = Book.find(params[:id])
     end
 
     def create
         @book = Book.new(book_params)
         @book.user_id = current_user.id
         @book.save
-        redirect_to books_path
+        redirect_to book_path(@book)
     end
 
     def destroy
     end
 
     def update
+        book = Book.find(params[:id])
+        book.update(book_params)
+        book.save
+        redirect_to book_path(book)
     end
-    
-    
+
+
     private
-    
+
     def book_params
         params.require(:book).permit(:title, :body)
     end
-    
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
     def index
+        @users = User.all
+        @book = Book.new
     end
     
     def show
-        
+        @user = User.find(params[:id])
+        @book = Book.new
     end
     
     def edit
