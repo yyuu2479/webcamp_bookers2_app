045f5ff2454a9c4b063091879ca726bd88fd6d65
yyuu2479 <yyuu2479@gmail.com>
2021-08-13 05:44:03 +0000

    Bookers2_follo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
+  has_many :fallower, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following_user, through: 'follower', source: :followed
+  
+  has_many :followed, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :follower_user, through: 'followed', source: :follower
+  
   attachment :profile_image
   
   validates :name, uniqueness: true, length:{in:2..20}
