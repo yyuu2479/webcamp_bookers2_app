66cb7373388054750bd3a3909675f0b7b61623cc
yyuu2479 <yyuu2479@gmail.com>
2021-08-25 02:13:56 +0000

    bookers2

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,28 +1,31 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     @book_comment = BookComment.new(book_comment_params)
     @book_comment.user_id = current_user.id
-    @book_comment.book_id = book.id
+    @book_comment.book_id = @book.id
     @book_comment.save
-    redirect_back fallback_location: root_path
+    
+    @book_comments = @book.book_comments
   end
-  
+
   def destroy
-    book_comment = BookComment.find(params[:id])
-    book_comment.destroy
-    redirect_back fallback_location: root_path
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
+    
+    @book_comments = @book.book_comments
   end
-  
-  
-  
+
+
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
-  
-  
+
+
+
 end
