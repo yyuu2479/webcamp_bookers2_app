6b8d6c8bedc8db1331cb25d4edc361857bec1c69
yyuu2479 <yyuu2479@gmail.com>
2021-08-26 07:42:50 +0000

    bookers2

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -13,12 +13,14 @@ class RelationshipsController < ApplicationController
 
   def create
     current_user.follow(params[:user_id])
-    redirect_back fallback_location: root_path
+    
+    @user = User.find(params[:user_id])
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_back fallback_location: root_path
+    
+    @user = User.find(params[:user_id])
   end
 
 end
