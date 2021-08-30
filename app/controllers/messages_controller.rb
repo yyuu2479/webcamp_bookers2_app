f4441f0b48b5f6caa7111a83324f7d806b89ecd2
yyuu2479 <yyuu2479@gmail.com>
2021-08-30 12:09:57 +0000

    bookers2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,8 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
-      redirect_to room_path(@message.room_id)
+      @room = @message.room
+      @messages = @room.messages
     else
       flash[:alert] = "メッセージが送信できませんでした！"
     end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,18 +8,33 @@ class RelationshipsController < ApplicationController
     user = User.find(params[:user_id])
     @users = user.follower_user
   end
-  
-  
+
+
 
   def create
     current_user.follow(params[:user_id])
-    
     @user = User.find(params[:user_id])
+
+    @current_user_entry = Entry.where(user_id: current_user.id)
+    @user_entry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_user_entry.each do |cu|
+        @user_entry.each do |u|
+          if cu.room_id == u.room_id
+            @is_room = true
+            @room_id = cu.room_id
+          end
+        end
+      end
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    
     @user = User.find(params[:user_id])
   end
 
